14f16d6628d495e96c5e21d3f911866c45f5369b
michalkwiatkowski <michal.kwiatkowski86@gmail.com>
2012-11-28 19:29:02 +0100

    rumsztyk_cmd.rb options and parameters parsing.

--- rumsztyk.rb
+++ rumsztyk.rb
@@ -1,6 +1,9 @@
 # TODO: finish definition
 class AllegroClient
-  def initialize(webapi_key)
+  def initialize(webapi_key, allegro_id, username, password)
     @webapi_key = webapi_key
+    @allegro_id = allegro_id
+    @username = username
+    @password = password
   end
 end
--- rumsztyk_cmd.rb
+++ rumsztyk_cmd.rb
@@ -9,37 +9,84 @@ require 'rumsztyk.rb'
 # Usage function
 def usage
   puts "USAGE:"
-  puts "./rumsztyk_cmd.rb -k ALLEGRO_WEBAPI_KEY"
+  puts "./rumsztyk_cmd.rb -e -k ALLEGRO_WEBAPI_KEY -l ALLEGRO_USERNAME -p ALLEGRO_PASSWORD -a ALLEGRO_ID"
   puts "or"
-  puts "./rumsztyk_cmd.rb --key ALLEGRO_WEBAPI_KEY"
+  puts "./rumsztyk_cmd.rb -execute --key ALLEGRO_WEBAPI_KEY --login ALLEGRO_USERNAME --password ALLEGRO_PASSWORD --allegro_id ALLEGRO_ID"
 end
 
 # Options and arguments parsing
 options = {}
+options[:username] = false
+options[:password] = false
+options[:allegro_id] = false
+options[:key] = false
+options[:execute] = false
+options[:usage] = false
+options[:help] = false
+
+# Script variables
+username = ''
+password = ''
+webkey = ''
+allegro_id = ''
+opts_ = ''
+
 optparse = OptionParser.new do |opts|
   
   # Display help
   opts.on('-h', '--help', 'Display help') do
-    puts opts
+    opts_ = opts
+    options[:help] = true
   end
   
   # Display usage
   opts.on('-u', '--usage', 'Display script usage') do
-    usage
+    options[:usage] = true
+  end
+  
+  # Specify allegro username
+  opts.on('-l', '--login USERNAME', 'Set allegro username') do |u|
+    username = u
+    options[:username] = true
+  end
+  
+  # Specify allegro password
+  opts.on('-p', '--password PASSWORD', 'Set allegro password') do |p|
+    password = p
+    options[:password] = true
+  end
+  
+  # Specify allegro ID
+  opts.on('-a', '--allegro_id ALLEGR_ID', 'Set allegro ID') do |a|
+    allegro_id = a
+    options[:allegro_id] = true
+  end
+  
+  # Specify allegro webapi key
+  opts.on('-k', '--key WEBAPI_KEY', 'Set allegro webapi key') do |key|
+    webkey = key
+    options[:key] = true
   end
   
   # Execute tests
-  opts.on('-k', '--key WEBAPI_KEY', 'Run tests with given Allegro.pl WEBAPI key') do |key|
-    # TODO:
-    # class definition
-    client = AllegroClient.new(key)
+  opts.on('-e', '--execute', 'Execute tests. Username, password, webapi key and allegro ID required.') do
+    options[:execute] = true
   end
   
 end
 
-# Parse options and arguments, execute code or catch exception and show usage
+# Parse options and arguments
 begin  
   optparse.parse!
 rescue
+  usage
+end
+
+# Execute tests
+if options[:execute] and options[:username] and options[:password] and options[:allegro_id] and options[:key]
+  client = AllegroClient.new(webkey, allegro_id, username, password)
+elsif options[:help]
+  puts opts_
+else
   usage
 end
